3f8c9889773db0fc7100b40190ffc987b108d6ac
Anna Voronkina <vau2007@rambler.ru>
2014-08-09 18:34:40 +0400

    ?

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,5 +2,5 @@ class Topic < ActiveRecord::Base
 	self.table_name = 'news'
 	enum status: {draft: 1, publication: 2}
 
-	scope :publications, -> {where(status: :publication)}
+	scope :publications, -> {where(status: 2)}
 end
