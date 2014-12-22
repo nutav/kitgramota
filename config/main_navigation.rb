ff5caac05d6a0102af18b85ee5cc092eed8c118f
Anna Voronkina <vau2007@rambler.ru>
2014-12-22 17:57:47 +0300

    +

--- config/main_navigation.rb
+++ config/main_navigation.rb
@@ -8,10 +8,17 @@ SimpleNavigation::Configuration.run do |navigation|
     primary.item :why, 'Зачем учить китайский?', why_chinese_path, highlights_on: -> {params[:controller]=='welcome' && (params[:action]=='why_chinese' || params[:action]=='chinese_for_kids' || params[:action]=='chinese_for_success' || params[:action]=='self_perfection' || params[:action] == 'union_joining' || params[:action] == 'up_to_date' || params[:action] == 'myths')}
     primary.item :articles, 'Статьи', publications_articles_path, highlights_on: -> {params[:controller]=='articles'}
     primary.item :courses, 'Курсы', courses_path, highlights_on: -> {params[:controller]=='education/levels' && (params[:action]=='courses' || params[:action]=='show')}
-    primary.item :galery, 'Галерея', gallery_path, highlights_on: -> {params[:controller]=='albums' || params[:controller]=='photos'}
-    primary.item :teachers, 'Лаоши', laoshi_path
+    primary.item :gallery, 'Галерея', gallery_path, highlights_on: -> {params[:controller]=='albums' || params[:controller]=='photos'}
+    primary.item :laoshi, 'Лаоши', laoshi_path
     primary.item :prices, 'Цены', prices_path
     primary.item :contacts, 'Контакты', contacts_path
     primary.item :partners, 'Партнёры', partners_path
+    if admin_signed_in?
+      primary.item :topics, 'Новости (а)', topics_path, highlights_on: -> {params[:controller]=='topics' && (params[:action]=='index' || params[:action]=='edit' || params[:action]=='new')}, class: 'admin_item'
+      primary.item :articles_a, 'Статьи (а)', articles_path, highlights_on: -> {params[:controller]=='articles' && (params[:action]=='index' || params[:action]=='edit' || params[:action]=='new')}, class: 'admin_item'
+      primary.item :prices_a, 'Цены (а)', all_prices_path, highlights_on: -> {params[:controller]=='prices' && (params[:action]=='index' || params[:action]=='edit' || params[:action]=='new')}, class: 'admin_item'
+      primary.item :teachers, 'Преподаватели (а)', teachers_path, highlights_on: -> {params[:controller]=='teachers' && (params[:action]=='index' || params[:action]=='edit' || params[:action]=='new')}, class: 'admin_item'
+      primary.item :albums, 'Фотографии (а)', albums_path, highlights_on: -> {params[:controller]=='albums' && (params[:action]=='index' || params[:action]=='edit' || params[:action]=='new')}, class: 'admin_item'
+    end
   end
 end
