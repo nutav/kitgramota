39c0c24cfca199abc3565f82438c4fef2d18fc78
Anna Voronkina <vau2007@rambler.ru>
2015-01-12 17:02:43 +0300

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,8 @@ set :application,   'kit-gramota'
 set :repo_url,      'git@github.com:nutav/kitgramota.git'
 set :deploy_to,     '/home/user/webapps/kit-gramota.ru'
 
+set :linked_files,  %w(.env)
+
 namespace :deploy do
 
   after :restart, :clear_cache do
