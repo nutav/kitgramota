9257daa3903308a00d2740c413a5c5fc0b158ac3
Anna Voronkina <vau2007@rambler.ru>
2015-01-20 15:57:21 +0300

    +

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,3 @@
 Rails.application.config.assets.precompile += %w( vendor/modernizr.js )
 Rails.application.config.assets.precompile += %w( vendor_foundation/my_fond.js )
-Rails.application.config.assets.precompile += %w( vendor_foundation/redactor/*.js )
+Rails.application.config.assets.precompile += %w( redactor/*.js )
