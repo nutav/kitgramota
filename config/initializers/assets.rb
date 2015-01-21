02bb762fa8fe0f816415998784f20f75f2c455d8
Anna Voronkina <vau2007@rambler.ru>
2015-01-21 14:23:04 +0300

    Яндекс.Метрика + редактор

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,2 +1,4 @@
 Rails.application.config.assets.precompile += %w( vendor/modernizr.js )
 Rails.application.config.assets.precompile += %w( vendor_foundation/my_fond.js )
+Rails.application.config.assets.precompile += %w( redactor/redactor.js )
+Rails.application.config.assets.precompile += %w( redactor/ru.js )
